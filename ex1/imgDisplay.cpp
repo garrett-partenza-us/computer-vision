a39649245485827a81b2aa9c15c76c6d6bb980e5
garrett-partenza-us <partenza.g@northeastern.edu>
2022-02-06 13:44:36 -0500

    start project two

--- ex1/imgDisplay.cpp
+++ ex1/imgDisplay.cpp
@@ -19,8 +19,8 @@ int main( int argc, char** argv ) {
       return -1;
     }
 
-  // cv::Mat greyimg(image.rows, image.cols, CV_8UC1);
-  // greyscale(image, greyimg);
+  cv::Mat greyimg(image.rows, image.cols, CV_8UC1);
+  greyscale(image, greyimg);
   // cv::Mat blurimg = cv::Mat(image.rows, image.cols, image.type());
   // blur5x5(image, blurimg);
   // cv::Mat xSobel = cv::Mat(image.rows, image.cols, CV_8UC3);
@@ -29,19 +29,22 @@ int main( int argc, char** argv ) {
   // sobelY3x3(image, ySobel);
   // cv::Mat magImg = cv::Mat(image.rows, image.cols, CV_8UC3);
   // magnitude(image, magImg);
-  // cv::Mat blurQuantImg = cv::Mat(image.rows, image.cols, CV_8UC3);
-  // int levels = 10;
-  // blurQuantize( image, blurQuantImg, levels );
+  cv::Mat blurQuantImg = cv::Mat(image.rows, image.cols, CV_8UC3);
   int levels = 10;
-  int threshold = 20;
-  cv::Mat cartoonImg = cv::Mat(image.rows, image.cols, CV_8UC3);
-  cartoon( image, cartoonImg, levels, threshold );
-
-
+  blurQuantize( image, blurQuantImg, levels );
+  //int levels = 10;
+  //int threshold = 20;
+  //cv::Mat cartoonImg = cv::Mat(image.rows, image.cols, CV_8UC3);
+  //cartoon( image, cartoonImg, levels, threshold );
+  
+  //cvtColor(image, image, CV_RGB2GRAY);
   cv::namedWindow( "Image Display", cv::WINDOW_AUTOSIZE );
   int l = 10;
   int t = 20;
-  cv::imshow( "Image", cartoonImg);
+  double contrastCoef = 0.5;
+  //cv::Mat contrastImg = cv::Mat(image.rows, image.cols, CV_8UC3);
+  //contrast(image, contrastImg, contrastCoef);
+  cv::imshow( "Image", greyimg);
   
   cv::waitKey(0);
   return 0;
